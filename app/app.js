7661f0c70940f1a2b3999faf62f95a3fa6a97346
Qianglong Mo <sammok2003@hotmail.com>
2014-12-23 18:14:58 +0800

    Create template files for Angular project.

---
+++ app/app.js
@@ -0,0 +1,8 @@
+// Yunen He 2014 (SukeBeta)
+
+var angularTemplate = angular.module("angularTemplate", ['ngMaterial']);
+
+angularTemplate.controller("angularTemplateCtrl", ['$scope','$http', function($scope, $http)
+{
+	// Something cool here!
+}]);
